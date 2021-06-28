2b5c08de5b74590185c3599f7e956990cc2725df
drewhollis <77352161+drewhollis@users.noreply.github.com>
2021-06-28 12:20:46 -0500

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "479859463274733",
+    clientSecret: "21a1b578de2c0aa3698148d319560aee",
+  },
+};
